15091785e6574b10abd6114b792369469c84680b
meelrossi <merossi@itba.edu.ar>
2017-05-14 20:22:59 -0300

    start elm project

---
+++ src/Main.elm
@@ -0,0 +1,38 @@
+module Main exposing (..)
+
+import Html exposing (program)
+import Msgs exposing (Msg)
+import Models exposing (Model, initialModel)
+import Update exposing (update)
+import View exposing (view)
+import Commands exposing (fetchPlayers)
+import Routing
+import Navigation exposing (Location)
+
+
+init : Location -> ( Model, Cmd Msg )
+init location =
+    let
+        currentRoute =
+            Routing.parseLocation location
+    in
+        ( initialModel currentRoute, fetchPlayers )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    Navigation.program Msgs.OnLocationChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
---
+++ src/Msgs.elm
@@ -0,0 +1,10 @@
+module Msgs exposing (..)
+
+import Models exposing (Player)
+import RemoteData exposing (WebData)
+import Navigation exposing (Location)
+
+
+type Msg
+    = OnFetchPlayers (WebData (List Player))
+    | OnLocationChange Location
